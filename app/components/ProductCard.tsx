6e8d290bdeee9a12ffc33738a75b8509af76ae77
Siddhant Pandey <pandeysiddhant21@gmail.com>
2024-01-03 16:28:48 +0530

    styling section

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import AddToCart from './AddToCart';
+import styles from './ProductCard.module.css';
 
 const ProductCard = () => {
     return (
-        <div>
+        <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
             <AddToCart />
         </div>
     );
