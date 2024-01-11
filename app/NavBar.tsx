1e89f4e5ab4fdac7c3e935df54ecb26429fa6142
Siddhant Pandey <pandeysiddhant21@gmail.com>
2024-01-11 14:57:13 +0530

    added auth

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <div className='flex bg-slate-200 p-5'>
+        <div className='flex bg-slate-200 p-5 space-x-3'>
             <Link href='/' className='mr-5'>
                 Next.js
             </Link>
             <Link href='/users' className='mr-5'>
                 Users
+            </Link>
+            <Link href='/api/auth/signin' className='mr-5'>
+                Login
             </Link>
         </div>
     );
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import NavBar from './NavBar';
-import { Suspense } from 'react';
+// import { Suspense } from 'react';
+import AuthProvider from './auth/provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,12 +20,14 @@ export default function RootLayout({
     return (
         <html lang='en' data-theme='winter'>
             <body className={inter.className}>
-                <NavBar />
-                <main className='p-5'>
-                    {/* <Suspense fallback={<p>Loading...</p>}> */}
-                    {children}
-                    {/* </Suspense> */}
-                </main>
+                <AuthProvider>
+                    <NavBar />
+                    <main className='p-5'>
+                        {/* <Suspense fallback={<p>Loading...</p>}> */}
+                        {children}
+                        {/* </Suspense> */}
+                    </main>
+                </AuthProvider>
             </body>
         </html>
     );
