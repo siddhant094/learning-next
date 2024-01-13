49ae932aa73efde2e487577dac96cafedfd9bda9
Siddhant Pandey <pandeysiddhant21@gmail.com>
2024-01-13 15:44:54 +0530

    added optimisations

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,42 @@
 'use client';
+import { useState } from 'react';
+import _ from 'lodash';
+
+// import dynamic from 'next/dynamic';
+// const HeavyComponent = dynamic(() => import('./components/HeavyComponent'), {
+//     loading: () => <p>Loading...</p>,
+// });
 // import Image from 'next/image';
 
-import Link from 'next/link';
-import ProductCard from './components/ProductCard';
-import { getServerSession } from 'next-auth';
-import { authOptions } from './api/auth/[...nextauth]/route';
-import Image from 'next/image';
-import mars from '@/public/images/mars.jpg';
-import { Metadata } from 'next';
-import HeavyComponent from './components/HeavyComponent';
+// import Link from 'next/link';
+// import ProductCard from './components/ProductCard';
+// import Image from 'next/image';
+// import mars from '@/public/images/mars.jpg';
+// import { Metadata } from 'next';
+// import { getServerSession } from 'next-auth';
+// import { authOptions } from './api/auth/[...nextauth]/route';
+// import HeavyComponent from './components/HeavyComponent';
 
-export default async function Home() {
-    const session = await getServerSession(authOptions);
+export default function Home() {
+    // const [isVisible, setIsVisible] = useState(false);
+    // const session = await getServerSession(authOptions);
     return (
         <main>
-            <h1 className='font-poppins'>
+            {/* <h1 className='font-poppins'>
                 Hello {session && <span>{session.user!.name}</span>}
-            </h1>
-            <HeavyComponent />
+            </h1> */}
+            <h1 className='font-poppins'>Hello World</h1>
+            <button
+                onClick={async () => {
+                    const _ = (await import('lodash')).default;
+                    const users = [{ name: 'c' }, { name: 'b' }, { name: 'a' }];
+                    const sorted = _.orderBy(users, ['name']);
+                    console.log(sorted);
+                }}
+            >
+                SHOW
+            </button>
+            {/* {isVisible && <HeavyComponent />} */}
             {/* <Link href='/users'>Users</Link> */}
             {/* <ProductCard /> */}
             {/* <Image
