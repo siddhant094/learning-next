9640358c4d4b3d27fc1f07d4cbc563130eed290c
Siddhant Pandey <pandeysiddhant21@gmail.com>
2024-01-13 11:53:02 +0530

    added emails

---
+++ app/api/send-email/route.tsx
@@ -0,0 +1,16 @@
+import WelcomeTemplate from '@/emails/WelcomeTemplate';
+import { NextResponse } from 'next/server';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+    resend.emails.send({
+        from: '...',
+        to: 'pandeysiddhant21@gmail.com',
+        subject: '...',
+        react: <WelcomeTemplate name='Mosh' />,
+    });
+
+    return NextResponse.json({});
+}
